fdc7a3a8b290b02294fa166edd81e365f3578816
Muhammad Rashid <mrashid@ucdavis.edu>
2018-11-23 20:59:42 -0800

    create first component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Contacts from './contact.jsx'
 
 class App extends React.Component {
   render() {
     return (
-      <div> Hello, this is mini project TEST</div>
+      <div> 
+        <h1>The App Component</h1>
+        <Contacts />
+      </div>
     );
   }
 }
 
-export default App
+export default App;
---
+++ client/src/components/contact.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Contacts extends Component {
+  render () {
+
+    return (
+      <div>
+        <h4> John Doe </h4>
+          <ul>
+            <li> Email: jdoe@yahoo.com </li>
+            <li> Phone: 111-111-1111   </li>
+          </ul>
+      </div>
+    );
+
+  }
+}
+
+export default Contacts;
